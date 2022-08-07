4f5927eecc4d500d0ccb8a6caf7dcff0ac3c2133
jhyunlee <jhyunlee@ubuntu18.04>
2022-08-07 12:24:19 +0900

    webserver

--- webServerSimple/main.go
+++ webServerSimple/main.go
@@ -13,35 +13,26 @@ var counter int
 var mutex = &sync.Mutex{}
 
 func echoString(w http.ResponseWriter, r *http.Request) {
-	log.Printf("call..[%+v]",r)
+	log.Printf("[%s][%s][%s][%v]", r.Method, r.Proto, r.URL.Path, r.Header)
 	fmt.Fprintf(w, "hello  : ")
 	mutex.Lock()
 	counter++
-	fmt.Fprintf(w, strconv.Itoa(counter))
+	time.Sleep(time.Millisecond * 100)
 	mutex.Unlock()
-
+	fmt.Fprintf(w, strconv.Itoa(counter))
 	//time.Sleep(time.Second * 1)
-	time.Sleep(time.Millisecond * 10)
-}
-
-func incrementCounter(w http.ResponseWriter, r *http.Request) {
-	mutex.Lock()
-	counter++
-	fmt.Fprintf(w, strconv.Itoa(counter))
-	mutex.Unlock()
+	log.Printf("[%s]", strconv.Itoa(counter))
 }
 
 func main() {
-	http.HandleFunc("/", echoString)
-
-	//http.Handle("/", http.FileServer(http.Dir("./static")))
-
-	http.HandleFunc("/increment", incrementCounter)
-
-	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", echoString)
+	mux.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hi")
 	})
 
-	log.Fatal(http.ListenAndServe(":8081", nil))
-
+	server := &http.Server{Addr: ":8081", Handler: mux}
+	//server.SetKeepAlivesEnabled(false)
+	server.ListenAndServe()
+	//http.ListenAndServe(":8081", nil)
 }
