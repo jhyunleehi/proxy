f6b691a37bc7df092e250913532bcf950ce511f5
jhyunlee <jhyunlee@ubuntu18.04>
2022-08-07 12:25:04 +0900

    proxy

--- rproxy1/main.go
+++ rproxy1/main.go
@@ -1,66 +1,67 @@
 package main
- 
+
 import (
-    "errors"
-    "fmt"
-    "log"
-    "net/http"
-    "net/http/httputil"
-    "net/url"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
 )
- 
+
 // NewProxy takes target host and creates a reverse proxy
 func NewProxy(targetHost string) (*httputil.ReverseProxy, error) {
-    url, err := url.Parse(targetHost)
-    if err != nil {
-        return nil, err
-    }
- 
-    proxy := httputil.NewSingleHostReverseProxy(url)
- 
-    originalDirector := proxy.Director
-    proxy.Director = func(req *http.Request) {
-        originalDirector(req)
-        modifyRequest(req)
-    }
- 
-    proxy.ModifyResponse = modifyResponse()
-    proxy.ErrorHandler = errorHandler()
-    return proxy, nil
+	url, err := url.Parse(targetHost)
+	if err != nil {
+		return nil, err
+	}
+
+	proxy := httputil.NewSingleHostReverseProxy(url)
+
+	originalDirector := proxy.Director
+	proxy.Director = func(req *http.Request) {
+		originalDirector(req)
+		modifyRequest(req)
+	}
+
+	// proxy.ModifyResponse = modifyResponse()
+	// proxy.ErrorHandler = errorHandler()
+	return proxy, nil
 }
- 
+
 func modifyRequest(req *http.Request) {
-    req.Header.Set("X-Proxy", "Simple-Reverse-Proxy")
+	req.Header.Set("X-Proxy", "Simple-Reverse-Proxy")
 }
- 
+
 func errorHandler() func(http.ResponseWriter, *http.Request, error) {
-    return func(w http.ResponseWriter, req *http.Request, err error) {
-        fmt.Printf("Got error while modifying response: %v \n", err)
-        return
-    }
+	return func(w http.ResponseWriter, req *http.Request, err error) {
+		fmt.Printf("Got error while modifying response: %v \n", err)
+		return
+	}
 }
- 
+
 func modifyResponse() func(*http.Response) error {
-    return func(resp *http.Response) error {
-        return errors.New("response body is invalid")
-    }
+	return func(resp *http.Response) error {
+		return errors.New("response body is invalid")
+	}
 }
- 
+
 // ProxyRequestHandler handles the http request using proxy
 func ProxyRequestHandler(proxy *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) {
-    return func(w http.ResponseWriter, r *http.Request) {
-        proxy.ServeHTTP(w, r)
-    }
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("[%s][%s][%s][%v]", r.Method, r.Proto, r.URL.Path, r.Header)
+		proxy.ServeHTTP(w, r)
+	}
 }
- 
+
 func main() {
-    // initialize a reverse proxy and pass the actual backend server url here
-    proxy, err := NewProxy("http://my-api-server.com")
-    if err != nil {
-        panic(err)
-    }
- 
-    // handle all requests to your server using the proxy
-    http.HandleFunc("/", ProxyRequestHandler(proxy))
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	// initialize a reverse proxy and pass the actual backend server url here
+	proxy, err := NewProxy("http://192.168.57.31:8081")
+	if err != nil {
+		panic(err)
+	}
+
+	// handle all requests to your server using the proxy
+	http.HandleFunc("/", ProxyRequestHandler(proxy))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
